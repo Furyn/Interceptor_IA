3cfb7ee81be4b2d46157f09bba204bb5bc80ef5f
SpaceCowboy2 <julien.dor@edu.devinci.fr>
2021-10-13 11:33:07 +0200

    Added Check Asteroid

--- Assets/Teams/Interceptor/CheckAsteroid.cs
+++ Assets/Teams/Interceptor/CheckAsteroid.cs
@@ -8,12 +8,28 @@ namespace Interceptor
     [TaskCategory("Interceptor")]
     public class CheckAsteroid : Action
     {
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Target Pos")]
+        public SharedVector2 target;
+
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Ship pos")]
+        public SharedVector2 myShip;
+
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("myship orientation")]
+        public SharedFloat myShipOrientation;
+
         [BehaviorDesigner.Runtime.Tasks.Tooltip("Asteroid in between")]
         public SharedBool asteroidInBetween;
 
-        //public RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
         public override TaskStatus OnUpdate()
         {
+            float shootAngle = Mathf.Deg2Rad * myShipOrientation.Value;
+
+            RaycastHit2D hit = Physics2D.Raycast(myShip.Value, new Vector2(Mathf.Cos(shootAngle), Mathf.Sin(shootAngle)));
+
+            if (hit.collider.CompareTag("Asteroid"))
+            {
+                asteroidInBetween.SetValue(true);
+            }
 
             return TaskStatus.Success;
         }

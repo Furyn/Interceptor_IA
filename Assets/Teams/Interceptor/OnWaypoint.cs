2116799b6b88073fbd503252fe8f156c776e58b8
Benzak98 <57401165+Benzak98@users.noreply.github.com>
2021-10-13 15:04:57 +0200

    [FEAT] Add Waypoint Script

---
+++ Assets/Teams/Interceptor/OnWaypoint.cs
@@ -0,0 +1,21 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+namespace Interceptor
+{
+    [TaskCategory("Interceptor")]
+    public class OnWaypoint : Action
+    {
+        public SharedBool onWayPoint;
+
+
+        public override TaskStatus OnUpdate()
+        {
+            if(onWayPoint.Value)
+                return TaskStatus.Success;
+            return TaskStatus.Failure;
+        }
+
+    }
+}

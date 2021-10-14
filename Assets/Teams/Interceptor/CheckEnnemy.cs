a8c072a29a87fbf27fba3f9ec75a0294d0f1e1a2
Benzak <dovbenzak2@hotmail.com>
2021-10-14 17:11:04 +0200

    [FEAT] CheckEnnemy (CtrlC + V)

---
+++ Assets/Teams/Interceptor/CheckEnnemy.cs
@@ -0,0 +1,36 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+using DoNotModify;
+
+namespace Interceptor
+{
+    [TaskCategory("Interceptor")]
+    public class CheckEnnemy : Conditional
+    {
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Target Pos")]
+        public SharedVector2 target;
+
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Ship pos")]
+        public SharedVector2 myShip;
+
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("Distance From Enemy")]
+        public SharedFloat DistanceFromEnemy;
+
+        public override TaskStatus OnUpdate()
+        {
+            Vector2 shootAngle = target.Value - myShip.Value;
+            int layerMask = 1 << 10;
+            RaycastHit2D hit = Physics2D.Raycast(myShip.Value, shootAngle, DistanceFromEnemy.Value, layerMask);
+            if (hit)
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    return TaskStatus.Failure;
+                }
+            }
+            return TaskStatus.Success;
+
+        }
+    }
+}
